c2cffc7f940dde527f8b331092c69e360def5683
Mael Lacour <public.dev@maellacour.com>
2024-06-09 20:43:44 +0200

    remove hero headline

--- pages/index.vue
+++ pages/index.vue
@@ -16,34 +16,6 @@ useSeoMeta({
       :description="page.hero.description"
       :links="page.hero.links"
     >
-      <template #headline>
-        <UBadge
-          v-if="page.hero.headline"
-          variant="subtle"
-          size="lg"
-          class="relative rounded-full font-semibold"
-        >
-          <NuxtLink
-            :to="page.hero.headline.to"
-            target="_blank"
-            class="focus:outline-none"
-            tabindex="-1"
-          >
-            <span
-              class="absolute inset-0"
-              aria-hidden="true"
-            />
-          </NuxtLink>
-
-          {{ page.hero.headline.label }}
-
-          <UIcon
-            v-if="page.hero.headline.icon"
-            :name="page.hero.headline.icon"
-            class="ml-1 w-4 h-4 pointer-events-none"
-          />
-        </UBadge>
-      </template>
 
       <ImagePlaceholder />
 
@@ -63,7 +35,6 @@ useSeoMeta({
     <ULandingSection
       :title="page.features.title"
       :description="page.features.description"
-      :headline="page.features.headline"
     >
       <UPageGrid
         id="features"
