6101dc4c99dd2b7772346d19a62c4c70eb4b9fda
Mael Lacour <public.dev@maellacour.com>
2024-06-09 22:41:45 +0200

    update footer

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -55,42 +55,9 @@ function onSubmit() {
 
 <template>
   <UFooter>
-    <template #top>
-      <UFooterColumns :links="links">
-        <template #right>
-          <form @submit.prevent="onSubmit">
-            <UFormGroup
-              label="Subscribe to our newsletter"
-              :ui="{ container: 'mt-3' }"
-            >
-              <UInput
-                v-model="email"
-                type="email"
-                placeholder="Enter your email"
-                :ui="{ icon: { trailing: { pointer: '' } } }"
-                required
-                size="xl"
-                autocomplete="off"
-                class="max-w-sm"
-              >
-                <template #trailing>
-                  <UButton
-                    type="submit"
-                    size="xs"
-                    :label="loading ? 'Subscribing' : 'Subscribe'"
-                    :loading="loading"
-                  />
-                </template>
-              </UInput>
-            </UFormGroup>
-          </form>
-        </template>
-      </UFooterColumns>
-    </template>
-
     <template #left>
       <p class="text-gray-500 dark:text-gray-400 text-sm">
-        Copyright © {{ new Date().getFullYear() }}. All rights reserved.
+        © {{ new Date().getFullYear() }} Mael Lacour
       </p>
     </template>
 
