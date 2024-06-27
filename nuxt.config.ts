948ba35ebe2e2513f311908399940198c77e61d9
Mael Lacour <public.dev@maellacour.com>
2024-06-27 15:47:53 +0200

    Add seo management

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,8 +6,15 @@ export default defineNuxtConfig({
     '@nuxt/eslint',
     '@nuxt/ui',
     '@nuxt/fonts',
-    '@nuxtjs/cloudinary'
+    '@nuxtjs/cloudinary',
+    '@nuxtjs/seo'
   ],
+  site: {
+    url: 'https://portfolio.maellacour.com',
+    name: 'Mael Lacour | Portfolio',
+    description: 'A portfolio where I showcase my work on web and Unity development.',
+    defaultLocale: 'en' // not needed if you have @nuxtjs/i18n installed
+  },
   ui: {
     icons: ['heroicons', 'simple-icons']
   },
