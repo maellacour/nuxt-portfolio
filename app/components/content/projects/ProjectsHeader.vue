2de73a221983aa02c7c158e4b9c9fd9d2344c93f
Mael Lacour <public.dev@maellacour.com>
2025-05-06 15:46:47 +0200

    fix: update props usage in ProjectsHeader component

--- app/components/content/projects/ProjectsHeader.vue
+++ app/components/content/projects/ProjectsHeader.vue
@@ -1,52 +1,42 @@
 <template>
   <div>
     <h2 class="text-5xl font-bold tracking-tight text-gray-900 dark:text-white sm:text-7xl">
-      Project: {{ title }}
+      Project: {{ props.title }}
     </h2>
     <div class="flex">
       <div class="flex items-center mr-10">
-        <span
-          class="i-heroicons-clock flex-shrink-0 h-6 w-6"
-          aria-hidden="true"
-        />
-        <span
-          class="
+        <span class="i-heroicons-clock flex-shrink-0 h-6 w-6" aria-hidden="true" />
+        <span class="
                   font-general-medium
                   ml-2
                   leading-none
                   text-primary-dark
                   dark:text-primary-light
-                "
-        >{{ publishDate }}</span>
+                ">{{ props.publishDate }}</span>
       </div>
       <div class="flex items-center">
-        <span
-          class="i-heroicons-tag flex-shrink-0 h-6 w-6"
-          aria-hidden="true"
-        />
-        <span
-          class="
+        <span class="i-heroicons-tag flex-shrink-0 h-6 w-6" aria-hidden="true" />
+        <span class="
                   font-general-medium
                   ml-2
                   leading-none
                   text-primary-dark
                   dark:text-primary-light
-                "
-        >{{ tag }}</span>
+                ">{{ props.tag }}</span>
       </div>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-defineProps({
+const props = defineProps({
   title: {
     type: String,
     default: 'Default title'
   },
   publishDate: {
     type: String,
-    default: Date.now().toString()
+    default: Date.now().toLocaleString()
   },
   tag: {
     type: String,
